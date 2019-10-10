2900df5773dd01fb02acd563b7f2fd510191b9d8
Jose Manuel <you@example.com>
2019-10-10 13:28:29 -0500

    Finish commit

--- src/App.js
+++ src/App.js
@@ -29,48 +29,69 @@ class ModalWrapper extends React.Component {
     super();
 
     this.state = {
-      modalIsOpen: false
+      modalIsOpen: false,
+      name: '',
+      img: '',
     };
-
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
     this.openModal = this.openModal.bind(this);
-    this.afterOpenModal = this.afterOpenModal.bind(this);
     this.closeModal = this.closeModal.bind(this);
+    this.addPiece = this.addPiece.bind(this);
+  }
+  handleChange(e) {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(event) {
+    let tempArray = JSON.parse(localStorage.getItem('obras'));
+    var newPiece = { name: this.state.name, img: this.state.img }
+    tempArray.push(newPiece);
+    localStorage.setItem("obras", JSON.stringify(tempArray));
+    alert('Submitted ' + this.state.name);
   }
 
   openModal() {
     this.setState({ modalIsOpen: true });
   }
-
-  afterOpenModal() {
-    // references are now sync'd and can be accessed.
-    this.subtitle.style.color = '#f00';
-  }
-
   closeModal() {
     this.setState({ modalIsOpen: false });
+  }
+
+  addPiece() {
+    let tempArray = JSON.parse(localStorage.getItem('obras'));
+    let newPiece = { name: this.state.name, img: this.state.img }
+    const obras = [...tempArray];
+    localStorage.setItem("obras", JSON.stringify(obras));
+    console.log('saved!');
   }
 
   render() {
     return (
       <div>
-        <button onClick={this.openModal}>Open Modal</button>
+        <button className='btn btn-red' onClick={this.openModal}>Add Piece</button>
         <Modal
           isOpen={this.state.modalIsOpen}
-          onAfterOpen={this.afterOpenModal}
           onRequestClose={this.closeModal}
           style={customStyles}
           contentLabel="Example Modal"
         >
-
-          <h2 ref={subtitle => this.subtitle = subtitle}>Hello</h2>
+          <h2>Add  a new Piece</h2>
           <button onClick={this.closeModal}>close</button>
-          <div>I am a modal</div>
-          <form>
-            <input />
-            <button>tab navigation</button>
-            <button>stays</button>
-            <button>inside</button>
-            <button>the modal</button>
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Name:
+          <input type="text" value={this.state.name} name="name" onChange={this.handleChange} />
+            </label>
+            <br />
+            <label>
+              img:
+          <input type="text" value={this.state.img} name="img" onChange={this.handleChange} />
+            </label>
+            <input type="submit" value="Submit" />
           </form>
         </Modal>
       </div>
@@ -81,7 +102,6 @@ class ModalWrapper extends React.Component {
 class Piece extends React.Component {
   render() {
     return (
-
       <AppContext.Consumer>
         {value => {
           return (
@@ -101,44 +121,6 @@ class Piece extends React.Component {
   }
 }
 
-
-class Form extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { nameValue: '', imgValue: '' };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({ value: event.target.value });
-  }
-
-  handleSubmit(event) {
-    alert('submitted: ' + this.state);
-    event.preventDefault();
-  }
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Name:
-          <input type="text" value={this.state.nameValue} onChange={this.handleChange} />
-        </label>
-        <br />
-        <label>
-          img:
-          <input type="text" value={this.state.imgValue} onChange={this.handleChange} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-    );
-  }
-}
-
-
 class App extends React.Component {
   constructor() {
     super();
@@ -151,13 +133,17 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    this.addPieces();
+
     this.getPieces();
   }
   addPieces() {
-    const obras = [{ name: 'La Odisea', img: 'http://bit.ly/2OycArf' }, { name: 'La Cabaña del Tío Tom', img: 'http://bit.ly/35qoxVJ' }, { name: 'Frankenstein', img: 'http://bit.ly/33mc2sF' }];
-    localStorage.setItem("obras", JSON.stringify(obras));
-    console.log('Seed Pieces saved!');
+    let tempArray = JSON.parse(localStorage.getItem('obras'))
+    if (tempArray.length == 0) {
+      const obras = [{ name: 'La Odisea', img: 'http://bit.ly/2OycArf' }, { name: 'La Cabaña del Tío Tom', img: 'http://bit.ly/35qoxVJ' }, { name: 'Frankenstein', img: 'http://bit.ly/33mc2sF' }];
+      localStorage.setItem("obras", JSON.stringify(obras));
+      console.log('Seed Pieces saved!');
+    }
+
   }
   getPieces() {
     let tempArray = JSON.parse(localStorage.getItem('obras'));
@@ -173,8 +159,8 @@ class App extends React.Component {
         }}>
           <NavigationBar />
           <MainTitle title={"Artmix"}></MainTitle>
-          <Piece></Piece>
           <ModalWrapper></ModalWrapper>
+          <Piece></Piece>
         </AppContext.Provider>
       </div>
     );
